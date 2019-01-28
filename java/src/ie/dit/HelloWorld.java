bfe0cb322a9424f71bd787b29832163742aaca7b
skooter500 <skooter500@gmail.com>
2019-01-28 12:53:49 +0000

    Added java class example

--- java/src/ie/dit/HelloWorld.java
+++ java/src/ie/dit/HelloWorld.java
@@ -4,5 +4,24 @@ public class HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("Hello world");    
+
+        Animal misty = new Dog(); // Polymorphism
+
+        misty.setName("Misty");
+        System.out.println(misty.getName());  
+        misty.speak();  
+
+        misty = new Cat();
+        misty.speak();
+
+        Animal garfield = new Cat();
+        garfield.setName("Garfield");
+
+        misty = garfield;
+
+        garfield.setName("Felix");
+
+        System.out.println(misty.getName());
+
     }
 }
