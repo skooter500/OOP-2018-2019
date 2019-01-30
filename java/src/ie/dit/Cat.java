2a0a85e0c9e8fc2a6d809c7f3a1a096053d704b7
skooter500 <skooter500@gmail.com>
2019-01-30 13:31:35 +0000

    bla

---
+++ java/src/ie/dit/Cat.java
@@ -0,0 +1,9 @@
+package ie.dit;
+
+public class Cat extends Animal
+{
+    public void speak()
+    {
+        System.out.println("Meow!");
+    }
+}
---
+++ java/src/ie/dit/Dog.java
@@ -0,0 +1,9 @@
+package ie.dit;
+
+public class Dog extends Animal
+{
+    public void speak()
+    {
+        System.out.println("Woof!");
+    }
+}
