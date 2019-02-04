3c82ce4011fbb2d51e41f61d93fe0e1de617a3cf
skooter500 <skooter500@gmail.com>
2019-02-04 10:52:54 +0000

    stuff for the class today

---
+++ java/src/ie/dit/HelloProcessing.java
@@ -0,0 +1,18 @@
+package ie.dit;
+
+import processing.core.PApplet;
+
+public class HelloProcessing extends PApplet
+{	
+	public void settings()
+	{
+		size(500, 500);
+	}
+
+	public void setup() {
+	}
+
+	public void draw() {
+		line(10, 10, 100, 100);		
+	}	
+}
