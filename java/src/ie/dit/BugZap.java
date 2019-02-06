15d29977f69b7cfc3facf85c5faca0ab798e90e9
skooter500 <skooter500@gmail.com>
2019-02-06 15:23:05 +0000

    bla

--- java/src/ie/dit/BugZap.java
+++ java/src/ie/dit/BugZap.java
@@ -10,7 +10,7 @@ public class BugZap extends PApplet
 	}
 
 	public void setup() {
-
+		reset();
 	}
 
 	/*
@@ -27,35 +27,116 @@ public class BugZap extends PApplet
 	}	
 	*/
 
-	float playerX = 250, playerY = 400;
+	float playerX, playerY;
+	float playerSpeed = 5;
 	float playerWidth = 40;
+	float halfPlayerWidth = playerWidth / 2;
+	
+
+	float bugX, bugY, bugWidth = 30;
+	float halfBugWidth = bugWidth / 2;
+
+	void reset()
+	{
+		resetBug();
+		playerX = width / 2;
+		playerY = height - 50;
+	}
+
+	void resetBug()
+	{
+		bugX = random(halfBugWidth, width - halfBugWidth);
+		bugY = 50;
+	}
+
+	void drawBug(float x, float y)
+	{
+		// Draw the bug
+		stroke(255);
+		float saucerHeight = bugWidth * 0.7f;
+		line(x, y - saucerHeight, x - halfBugWidth, y);
+		line(x, y - saucerHeight, x +  halfBugWidth, y);		
+		line(x - halfBugWidth, y, x - halfBugWidth, y);
+		line(x - halfBugWidth, y, x + halfBugWidth, y);
+		float feet = bugWidth * 0.1f;
+		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
+		line(x  + feet, y, x + halfBugWidth, y + halfBugWidth);
+
+		
+		feet = bugWidth * 0.3f;
+		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
+		line(x  + feet, y, x + halfBugWidth, y + halfBugWidth);
+
+		float eyes  = bugWidth * 0.1f;
+		line(x - eyes, y - eyes, x - eyes, y - eyes * 2f);
+		line(x + eyes, y - eyes, x + eyes, y - eyes * 2f);
+	
+	}
 
 	void drawPlayer(float x, float y, float w)
 	{
 		stroke(255);
 		float playerHeight = w / 2;
-		line(playerX, playerY + playerHeight, playerX + playerWidth, playerY + playerHeight);
-		line(playerX, playerY + playerHeight, playerX, playerY + playerHeight * 0.5f);
-		line(playerX + playerWidth, playerY + playerHeight, playerX + playerWidth, playerY + playerHeight * 0.5f);
+		line(x - halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight);
+		line(x - halfPlayerWidth, y + playerHeight, x - halfPlayerWidth, y + playerHeight * 0.5f);
+		line(x + halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight * 0.5f);
 		
-		line(playerX, playerY + playerHeight * 0.5f, playerX + playerWidth * 0.2f, playerY + playerHeight * 0.3f);
-		line(playerX + playerWidth, playerY + playerHeight * 0.5f, playerX + playerWidth * 0.8f, playerY + playerHeight * 0.3f);
-		line(playerX + playerWidth * 0.2f, playerY + playerHeight * 0.3f, playerX + playerWidth * 0.8f, playerY + playerHeight * 0.3f);
-		line(playerX + playerWidth * 0.5f, playerY, playerX + playerWidth * 0.5f, playerY + playerHeight * 0.3f); 
+		
+		line(x - halfPlayerWidth, y + playerHeight * 0.5f, x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
+		line(x + halfPlayerWidth, y + playerHeight * 0.5f, x + (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
+		
+		line(x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f, x + (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
+		
+		line(x, y, x, y + playerHeight * 0.3f); 
+		
 	}
 
 	public void keyPressed()
 	{
 		if (keyCode == LEFT)
 		{
-			System.out.println("Left arrow pressed");
+			if (playerX > halfPlayerWidth)
+			{
+				playerX -= playerSpeed;
+			}
+		}
+		if (keyCode == RIGHT)
+		{
+			if (playerX < width - halfPlayerWidth)
+			{
+				playerX += playerSpeed;
+			}
+		}
+		if (key == ' ')
+		{
+			line(playerX, playerY, playerX, bugY);
 		}
 	}	
+
+	void moveBug()
+	{
+		if ((frameCount % 60) == 0)
+		{
+			bugX += random(-5, +5);
+			if (bugX < halfBugWidth )
+			{
+			  bugX = 0;
+			}
+			
+			if (bugX + halfBugWidth > width)
+			{
+			  bugX = width - halfBugWidth;
+			}
+			bugY ++;
+		}
+	}
 
 	public void draw()
 	{	
 		background(0);		
 		fill(255);
 		drawPlayer(playerX, playerY, playerWidth);
+		drawBug(bugX, bugY);
+		moveBug();
 	}
 }
