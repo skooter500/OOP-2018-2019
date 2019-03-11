15a5370954edad9129e4b33e4533aee038249203
skooter500 <skooter500@gmail.com>
2019-03-11 12:57:51 +0000

    Ship class

--- java/src/ie/dit/Ship.java
+++ java/src/ie/dit/Ship.java
@@ -5,14 +5,18 @@ import processing.core.PVector;
 public class Ship
 {
     private PVector pos;
+    private PVector forward;
     private float speed;
     private float size;
     private YASC yasc;
+
+    private float rotation;
 
     public Ship(YASC yasc, float x, float y, float speed, float size)
     {
         this.yasc = yasc;
         pos = new PVector(x, y);
+        forward = new PVector(0, -1);
         this.speed = speed;
         this.size = size;
 
@@ -20,12 +24,45 @@ public class Ship
 
     public void render()
     {
+        yasc.translate(pos.x, pos.y);
+        yasc.rotate(rotation);
         
+        float halfSize = size / 2;
+        yasc.line(- halfSize, halfSize, 0, - halfSize);
+        yasc.line(0, - halfSize
+        , halfSize, halfSize);
+
+        yasc.line(halfSize, halfSize
+            ,0, 0);
+        yasc.line(0, 0, -halfSize, halfSize);
+
     }
 
     public void update()
     {
+        forward.x = yasc.sin(rotation);
+        forward.y = - yasc.cos(rotation);
+        if (yasc.checkKey('w'))
+        {
+            pos.x += forward.x;
+            pos.y += forward.y;
+        }
 
+        if (yasc.checkKey('s'))
+        {
+            pos.x -= forward.x;
+            pos.y -= forward.y;
+        }
+
+        if (yasc.checkKey('a'))
+        {
+            rotation -= 0.1f;
+        }
+
+        if (yasc.checkKey('d'))
+        {
+            rotation += 0.1f;
+        }
     }
 
 
@@ -67,6 +104,21 @@ public class Ship
     /**
      * @param size the size to set
      */
+    
+
+	/**
+	 * @return the yasc
+	 */
+	public YASC getYasc() {
+		return yasc;
+	}
+
+	/**
+	 * @param yasc the yasc to set
+	 */
+	public void setYasc(YASC yasc) {
+		this.yasc = yasc;
+    }
     public void setSize(float size) {
         this.size = size;
     }
--- java/src/ie/dit/YASC.java
+++ java/src/ie/dit/YASC.java
@@ -5,6 +5,24 @@ import processing.core.PApplet;
 public class YASC extends PApplet
 {
     Ship s;
+    boolean[] keys = new boolean[1024];
+
+    public void keyPressed()
+    {
+        keys[keyCode] = true;
+    }
+    public void keyReleased()
+    {
+        keys[keyCode] = false;
+    }
+
+    public boolean checkKey(int c)
+    {
+        return keys[c] || keys [Character.toUpperCase(c)];
+    }
+
+
+
 
     public void settings()
     {
@@ -19,6 +37,7 @@ public class YASC extends PApplet
 
     public void draw()
     {
+        background(255);
         s.render();
         s.update();
     }
